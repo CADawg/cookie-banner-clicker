697535ee9ec20a908de7ada36dc5193d6e6df286
Conor <git@dbuidl.com>
2022-06-16 22:30:34 +0100

    Pre-ITx Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,134 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import webp from './photos/cookie.webp';
+import jpg from './photos/cookie.jpg';
+import {faTrophy} from '@fortawesome/free-solid-svg-icons/faTrophy';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+interface IPlayer {
+    name: string;
+    identifier: string;
+    score: number;
+}
+
+function GetNumberSuffix(number: number): string {
+    if (number === 1) {
+        return "st";
+    } else if (number === 2) {
+        return "nd";
+    } else if (number === 3) {
+        return "rd";
+    } else {
+        return "th";
+    }
+}
+
+function randomElementId(): string {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+function Checkbox(props: {label?: string, checked: boolean, setChecked: (checked: boolean) => void}) {
+    function voidAction(e: React.SyntheticEvent): false {
+        e.preventDefault();
+        e.stopPropagation();
+
+        return false;
+    }
+
+    let checkboxId = randomElementId();
+
+    return (
+        <form onSubmit={voidAction}>
+            <div className="checkbox">
+                <input type="checkbox" id={checkboxId} checked={props.checked} onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setChecked(e.target.checked)} />
+                <label htmlFor={checkboxId}>{props.label ? props.label : ""}</label>
+            </div>
+        </form>
+    );
+}
 
 function App() {
+  const [leaderboard, setLeaderboard] = React.useState<IPlayer[]>([
+      {name: "Player 1", identifier: "player1", score: 0},
+        {name: "Player 2", identifier: "player2", score: 0},
+        {name: "Player 3", identifier: "player3", score: 0},
+        {name: "Player 4", identifier: "player4", score: 0},
+        {name: "Player 5", identifier: "player5", score: 0},
+        {name: "Player 6", identifier: "player6", score: 0},
+        {name: "Player 7", identifier: "player7", score: 0},
+        {name: "Player 8", identifier: "player8", score: 0},
+        {name: "Player 9", identifier: "player9", score: 0},
+        {name: "Player 10", identifier: "player9", score: 0},
+  ]);
+
+  function Play(e: React.SyntheticEvent) {
+      e.preventDefault();
+        e.stopPropagation();
+
+      if (initialFailCheck) {
+         setIsNinePlusTenTwentyOne(true);
+
+        return;
+      }
+
+      setGameLevel(1);
+  }
+
+  const [currentScore, setCurrentScore] = React.useState(0);
+
+  const [initialFailCheck, setInitialFailCheck] = React.useState(true);
+
+  const [isNinePlusTenTwentyOne, setIsNinePlusTenTwentyOne] = React.useState(false);
+
+  const [gameLevel, setGameLevel] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+          <div className={"nav-brand"}>Cookie Clicker</div>
+        </nav>
+
+        <div className={"olay-70"}>
+            <div className={"olay-content"}>
+                <h1>Cookie (Banner) Clicker</h1>
+                <p>A really dumb idea by <a href={"https://github.com/Snaddyvitch-Dispenser"} rel="noreferrer" target={"_blank"}>Snaddyvitch-Dispenser</a></p>
+                <p>Click through as many cookie banners as you can <strong>without</strong> accepting <strong>any</strong> cookies.</p>
+
+                {/*checkbox*/}
+                <Checkbox label={"Accept Cookies"} checked={initialFailCheck} setChecked={(checked: boolean) => {setInitialFailCheck(checked)}} />
+
+                <button onClick={Play}>Let's Go!</button>
+                <p className={"error"} style={{display: isNinePlusTenTwentyOne ? "block" : "none"}}>You have to uncheck the "Accept Cookies" box!</p>
+            </div>
+        </div>
+
+
+        <div className={"container"}>
+            <picture className={"background"}>
+                <source srcSet={webp} type="image/webp" />
+                <source srcSet={jpg} type="image/jpeg" />
+                <img src={jpg} alt="cookie" />
+            </picture>
+
+            <div className={"leaderboard"}>
+                <div className={"leaderboard-header"}>
+                    <div className={"leaderboard-header-title"}>Leaderboard</div>
+                </div>
+
+                <div className={"leaderboard-body"}>
+                    {leaderboard.map((player, index) => (
+                        <div className={`leaderboard-body-player rank-${index + 1}`} key={index}>
+                            <span className={"trophy"}>{index < 3 ? <FontAwesomeIcon icon={faTrophy} className="trophy-white" /> : null}</span>
+                            <div className={"flex-player-info"}>
+                                <span className={`leaderboard-body-player-rank rank-${index + 1}`}>{(index + 1) + GetNumberSuffix(index + 1)}</span>
+                                <span className={"leaderboard-body-player-name"}>{player.name}</span>
+                                <span className={"leaderboard-body-player-score"}>{player.score}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/Constant.ts
@@ -0,0 +1,46 @@
+interface ICookie {
+    id: string;
+    name: string;
+    description: string;
+    https: boolean;
+    duration: string;
+    purpose_id: string;
+}
+
+interface ICompany {
+    id: string;
+    name: string;
+    cookies: ICookie[];
+}
+
+const COMPANIES: ICompany[] = [
+    {id: "intersussy", name: "InterSussy Studios", cookies: [{purpose_id: "marketing", id: "amogus", name: "X-Amogus", description: "Stores whether you have played the hit game Amogus", https: true, duration: "1 decade"}]},
+    {id: "giggle_analytics", name: "Giggle Analytics", cookies: [{purpose_id: "statistics", id: "ga-ui", name: "User ID", description: "A persistent ID which tracks the user across the web", https: true, duration: "1 year"}, {purpose_id: "statistics", id: "ga-ua", name: "Giggle Analytics User Agent", description: "The user agent used by Giggle Analytics", https: true, duration: "1 year"}]},
+    {id: "giggle_advertising", name: "Giggle Advertising", cookies: [{purpose_id: "giggle_personalisation", id: "ga-ui", name: "User ID", description: "A persistent ID which tracks the user across the web", https: true, duration: "1 year"}, {purpose_id: "marketing", id: "ga-ua", name: "Giggle Advertising User Agent", description: "The user agent used by Giggle Advertising", https: true, duration: "1 year"}]},
+    {id: "ian", name: "Integrated Ads Network", cookies: [{purpose_id: "marketing", id: "ian-user", name: "User ID", description: "A persistent ID which tracks the user across the web", https: true, duration: "1 year"}]},
+    {id: "hjeatmap", name: "Hjeatmap", cookies: [{purpose_id: "statistics", id: "hjeatmap-id", name: "Session ID", description: "An ID to report user clicks to Hjeatmap", https: true, duration: "Session"}]},
+    {id: "configgur", name: "Configgur", cookies: [{purpose_id: "other", id: "configgur-id", name: "Session ID", description: "An ID to identify users for A/B testing of configuration values", https: true, duration: "Session"}]},
+    {id: "php_session", name: "PHP Session", cookies: [{purpose_id: "functional", id: "PHPSESSID", name: "Session ID", description: "Remembers User Logins", https: true, duration: "1 month"}]},
+
+];
+
+interface IPurpose {
+    id: string;
+    name: string[];
+    description: string[];
+    required: boolean;
+    alwaysShow: boolean;
+}
+
+const PURPOSES: IPurpose[] = [
+    {id: "necessary", name: ["Necessary", "Information Storage and Access"], description: ["Cookies Required for the display and security of the website."], required: true, alwaysShow: true},
+    {id: "functional", name: ["Functional"], description: ["Cookies Required for full functioning of the website."], required: true, alwaysShow: false},
+    {id: "preferences", name: ["Preferences", "Personalisation"], description: ["Cookies used to store your preferences"], required: false, alwaysShow: true},
+    {id: "statistics", name: ["Statistics", "Measurement"], description: ["Cookies used to track visits and visitors to our website"], required: false, alwaysShow: true},
+    {id: "giggle_personalisation", name: ["Giggle Personalisation"], description: ["Personalisation by the Giggle Company"], required: false, alwaysShow: false},
+    {id: "marketing", name: ["Marketing", "Ad Selection"], description: ["Cookies used to serve you advertisements."], required: false, alwaysShow: true},
+    {id: "other", name: ["Other", "Third Party"], description: ["Other cookies"], required: false, alwaysShow: false}
+]
+
+export { COMPANIES, PURPOSES };
+export type { ICookie, ICompany, IPurpose };
